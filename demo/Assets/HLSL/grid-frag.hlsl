586df4f6a18a50f20814a14830d1f30e6f5b7465
Eric Mellino <erme@microsoft.com>
2017-08-30 00:04:02 -0700

    Better grid shader.

--- demo/Assets/HLSL/grid-frag.hlsl
+++ demo/Assets/HLSL/grid-frag.hlsl
@@ -4,16 +4,18 @@ struct PSInput
     float3 worldPosition : TEXCOORD0;
 };
 
+// Shader code from http://madebyevan.com/shaders/grid/
 
 float4 PS(PSInput input) : SV_Target
 {
-    if (frac(input.worldPosition.x / 10.0) < 0.05
-        || frac(input.worldPosition.z / 10.0) < 0.05)
-    {
-        return float4(1, 1, 1, 1);
-    }
-    else
-    {
-        return float4(0, 0, 0, 1);
-    }
+    // Pick a coordinate to visualize in a grid
+    float2 coord = input.worldPosition.xz;
+
+    // Compute anti-aliased world-space grid lines
+    float2 grid = abs(frac((coord / 100) - 0.5) - 0.5) / fwidth(coord);
+    float l = min(grid.x, grid.y) * 100;
+
+    // Just visualize the grid lines directly
+    float comp = 1.0 - min(l, 1.0);
+    return float4(float3(comp, comp, comp), 0.8);
 }
